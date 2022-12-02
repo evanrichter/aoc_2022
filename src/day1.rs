32f8101b0e0c82c30bd563cde71540d378efcf63
Evan Richter <evanjrichter@gmail.com>
2022-12-01 19:14:44 -0600

    day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,15 +1,38 @@
-#[derive(Clone, Copy)]
-pub struct Solver;
-
-impl super::Solve for Solver {
-    fn solve(&self, input: String) -> String {
-        // part 1
-        let depths: Vec<u32> = input.lines().map(|s| s.parse().unwrap()).collect();
-        let part1: u32 = depths.windows(2).filter(|w| w[1] > w[0]).count();
-
-        // part 2
-        let sums_of_three: Vec<u32> = depths.windows(3).map(|w| w.iter().sum()).collect();
-        let part2: u32 = sums_of_three.windows(2).filter(|w| w[1] > w[0]).count();
-        format!("{part1}, {part2}")
+pub fn solve(input: String) -> String {
+    // part 1
+    let calorie_stream: Vec<Option<u32>> = input.lines().map(|s| s.parse().ok()).collect();
+    let mut elves: Vec<Vec<u32>> = Vec::new();
+    let mut elf = Vec::new();
+    for c in calorie_stream {
+        match c {
+            Some(val) => elf.push(val),
+            None => {
+                // elf is done
+                elves.push(elf.clone());
+                elf.clear();
+            }
+        }
     }
+
+    // find biggest elf
+    let part1 = elves
+        .iter()
+        .map(|v| v.iter().sum::<u32>())
+        .max()
+        .expect("at least one elf");
+
+    // part 2
+
+    // collect the sums, then sort
+    let mut sums: Vec<u32> = elves.iter().map(|v| v.iter().sum::<u32>()).collect();
+    sums.sort_unstable();
+    sums.reverse();
+
+    // get the top 3
+    let &[a, b, c, ..] = sums.as_slice() else {
+            panic!("not even 3 elves?");
+        };
+    let part2 = a + b + c;
+
+    format!("{part1}, {part2}")
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,17 @@
 mod day1;
 
 #[rustfmt::skip]
-const SOLVERS: &[&dyn Solve] = &[
-    &day1::Solver,
+const SOLVERS: &[fn(String) -> String] = &[
+    day1::solve,
 ];
 
-pub trait Solve {
-    fn solve(&self, input: String) -> String;
-}
-
 pub fn solve_all() -> anyhow::Result<()> {
-    let aoc = emergence::AoC::new(2021)?;
+    let aoc = emergence::AoC::new(2022)?;
 
     for (day, solver) in SOLVERS.iter().enumerate() {
         let day = day + 1;
         let input = aoc.read_or_fetch(day)?;
-        let solution = solver.solve(input);
+        let solution = solver(input);
         println!("day {day} solution: {solution}");
     }
 
