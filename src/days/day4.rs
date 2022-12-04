a835061759af7f1f196b2572092b54df999aaf7e
Evan Richter <evanjrichter@gmail.com>
2022-12-03 23:52:30 -0600

    day 4

---
+++ src/days/day4.rs
@@ -0,0 +1,60 @@
+pub fn solve(input: String) -> String {
+    let part1 = part1(&input);
+    let part2 = part2(&input);
+    format!("{part1}, {part2}")
+}
+
+fn part1(input: &str) -> String {
+    let count = input
+        .trim()
+        .lines()
+        .map(ranges)
+        .filter_map(|(a, b)| (a.fully_contains(&b) || b.fully_contains(&a)).then(|| ()))
+        .count();
+    format!("{count}")
+}
+
+fn part2(input: &str) -> String {
+    let count = input
+        .trim()
+        .lines()
+        .map(ranges)
+        .filter_map(|(a, b)| a.overlaps(&b).then(|| ()))
+        .count();
+    format!("{count}")
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Range {
+    beg: u32,
+    end: u32,
+}
+
+impl Range {
+    fn parse(s: &str) -> Range {
+        let (beg, end) = s.split_once('-').unwrap();
+        Range {
+            beg: beg.parse().unwrap(),
+            end: end.parse().unwrap(),
+        }
+    }
+
+    fn fully_contains(&self, other: &Range) -> bool {
+        self.beg <= other.beg && self.end >= other.end
+    }
+
+    fn overlaps(&self, other: &Range) -> bool {
+        if self.end < other.beg {
+            return false;
+        }
+        if other.end < self.beg {
+            return false;
+        }
+        true
+    }
+}
+
+fn ranges(line: &str) -> (Range, Range) {
+    let (a, b) = line.split_once(',').unwrap();
+    (Range::parse(a), Range::parse(b))
+}
--- src/lib.rs
+++ src/lib.rs
@@ -21,4 +21,5 @@ const SOLVERS: &[fn(String) -> String] = &[
     day1::solve,
     day2::solve,
     day3::solve,
+    day4::solve,
 ];
