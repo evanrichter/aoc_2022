ee154a162f41dae2b202f200ee777e220c4e8e18
Evan Richter <evanjrichter@gmail.com>
2022-12-05 22:23:58 -0500

    nix flake and direnv support

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "A Nix-flake-based Rust development environment";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    { self
+    , flake-utils
+    , nixpkgs
+    , rust-overlay
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlays = [
+        (import rust-overlay)
+        (self: super: {
+          rustToolchain =
+            let
+              rust = super.rust-bin;
+            in
+            if builtins.pathExists ./rust-toolchain.toml then
+              rust.fromRustupToolchainFile ./rust-toolchain.toml
+            else if builtins.pathExists ./rust-toolchain then
+              rust.fromRustupToolchainFile ./rust-toolchain
+            else
+              rust.nightly.latest.default;
+        })
+      ];
+
+      pkgs = import nixpkgs { inherit system overlays; };
+      frameworks = pkgs.darwin.apple_sdk.frameworks;
+    in
+    {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          rustToolchain
+          rust-analyzer
+          cargo-watch
+          watchexec
+        ];
+
+        buildInputs = [ ]
+          # add MacOS frameworks
+          ++ pkgs.lib.optionals (pkgs.stdenv.isDarwin) (with frameworks; [
+          Security
+          CoreFoundation
+          CoreServices
+        ]);
+
+        shellHook = ''
+          ${pkgs.rustToolchain}/bin/cargo --version
+        '';
+      };
+    });
+}
