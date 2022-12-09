aeef2c6c9b96fef6a531f1b2e66ce0ac2b90fec5
Evan Richter <evanjrichter@gmail.com>
2022-12-09 01:47:23 -0600

    single binary benchmarks with const inputs

--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,21 @@ const SOLVERS: &[fn(&str) -> String] = &[
     day8::solve,
     day9::solve,
 ];
+
+const STATIC_INPUT_SOLVERS: &[(fn(&str) -> String, &str)] = &[
+    (day1::solve, include_str!("inputs/day01.txt")),
+    (day2::solve, include_str!("inputs/day02.txt")),
+    (day3::solve, include_str!("inputs/day03.txt")),
+    (day4::solve, include_str!("inputs/day04.txt")),
+    (day5::solve, include_str!("inputs/day05.txt")),
+    (day6::solve, include_str!("inputs/day06.txt")),
+    (day7::solve, include_str!("inputs/day07.txt")),
+    (day8::solve, include_str!("inputs/day08.txt")),
+    (day9::solve, include_str!("inputs/day09.txt")),
+];
+
+pub fn solve_with_static_input(day: usize) {
+    let (solver, input) = STATIC_INPUT_SOLVERS[day - 1];
+    let solution = solver(input);
+    println!("day {day} solution: {solution}");
+}
