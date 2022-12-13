3121022d9e70391f89244a62e210ce4777855a4a
Evan Richter <evanjrichter@gmail.com>
2022-12-12 22:36:20 -0600

    day 12 DontRepeatMyself

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -60,7 +60,10 @@ impl PartialOrd for Distance {
     }
 }
 
-fn part1(input: &str) -> usize {
+fn hike_the_mountain<F>(input: &str, mut check: F) -> usize
+where
+    F: FnMut(Option<(GridEntry, usize)>) -> ControlFlow,
+{
     let width = input.find('\n').unwrap();
     let height = input.chars().filter(|c| c == &'\n').count();
     let mut grid: Vec<Vec<GridEntry>> = input
@@ -132,10 +135,10 @@ fn part1(input: &str) -> usize {
     let mut queue = BTreeSet::from([grid[end_xy.0][end_xy.1]]);
 
     while let Some(current) = (&mut queue).pop_first() {
-        let current_distance = current.distance.unwrap();
-        if current.index == start_index {
-            // found the start! return the smallest distance (must already be found)
-            return current_distance;
+        match check(Some((current, start_index))) {
+            ControlFlow::Return(val) => return val,
+            ControlFlow::Continue => continue,
+            ControlFlow::FallThrough => {}
         }
 
         fn climbable(us: GridEntry, other: &mut GridEntry) -> bool {
@@ -191,148 +194,52 @@ fn part1(input: &str) -> usize {
         }
     }
 
-    panic!("didn't find the start")
+    let ControlFlow::Return(val) = check(None) else {
+        panic!("check closure didn't handle function end");
+    };
+
+    val
+}
+
+enum ControlFlow {
+    // stop searching the grid and return the value
+    Return(usize),
+    // like the `continue` keyword
+    Continue,
+    // keep going, don't divert control flow
+    FallThrough,
+}
+
+fn part1(input: &str) -> usize {
+    hike_the_mountain(input, |vals| {
+        let (current, start_index) = vals.unwrap();
+
+        if current.index == start_index {
+            // found the start! return the smallest distance (must already be found)
+            ControlFlow::Return(current.distance.unwrap())
+        } else {
+            ControlFlow::FallThrough
+        }
+    })
 }
 
 fn part2(input: &str) -> usize {
-    let width = input.find('\n').unwrap();
-    let height = input.chars().filter(|c| c == &'\n').count();
-    let mut grid: Vec<Vec<GridEntry>> = input
-        .lines()
-        .enumerate()
-        .map(|(y, l)| {
-            l.chars()
-                .enumerate()
-                .map(|(x, c)| (width * y + x, c as u8).into())
-                .collect()
-        })
-        .collect();
+    let mut best = usize::MAX;
+    hike_the_mountain(input, |vals| {
+        let Some((current, _)) = vals else {
+            return ControlFlow::Return(best);
+        };
 
-    let get_yx = |index: usize| {
-        let y = index / width;
-        let x = index % width;
-        (y, x)
-    };
-
-    let up = |index| {
-        let (y, x) = get_yx(index);
-        Some((y.checked_sub(1)?, x))
-    };
-
-    let down = |index| {
-        let (y, x) = get_yx(index);
-        if y + 1 >= height {
-            None
-        } else {
-            Some((y + 1, x))
-        }
-    };
-
-    let left = |index| {
-        let (y, x) = get_yx(index);
-        Some((y, x.checked_sub(1)?))
-    };
-
-    let right = |index| {
-        let (y, x) = get_yx(index);
-        if x + 1 >= width {
-            None
-        } else {
-            Some((y, x + 1))
-        }
-    };
-
-    let start_index = grid
-        .iter()
-        .flatten()
-        .position(|e| e.height == b'S')
-        .unwrap();
-    let end_index = grid
-        .iter()
-        .flatten()
-        .position(|e| e.height == b'E')
-        .unwrap();
-    let start_xy = get_yx(start_index);
-    let end_xy = get_yx(end_index);
-
-    // set start height to 'a'
-    grid[start_xy.0][start_xy.1].height = b'a';
-
-    // set end height to 'z' and a known distance to goal (0)
-    grid[end_xy.0][end_xy.1].height = b'z';
-    grid[end_xy.0][end_xy.1].distance = Known(0);
-
-    // priority queue starts with the end
-    let mut queue = BTreeSet::from([grid[end_xy.0][end_xy.1]]);
-
-    let mut a_paths = Vec::new();
-
-    while let Some(current) = (&mut queue).pop_first() {
-        let current_distance = current.distance.unwrap();
         if current.height == b'a' {
-            // found the start! return the smallest distance (must already be found)
-            a_paths.push(current_distance);
+            best = best.min(current.distance.unwrap());
 
             // we may be neighbor to other a paths! but we can continue because
             // this one is known to be better than them anyway
-            continue;
+            ControlFlow::Continue
+        } else {
+            ControlFlow::FallThrough
         }
-
-        fn climbable(us: GridEntry, other: &mut GridEntry) -> bool {
-            // could climb up one to us or could fall down to us
-            if us.height == other.height
-                || us.height == other.height + 1
-                || us.height < other.height
-            {
-                if other.distance == Unknown {
-                    other.distance = Known(us.distance.unwrap() + 1);
-                    return true;
-                } else {
-                    let od = other.distance.unwrap();
-                    let possible = us.distance.unwrap() + 1;
-                    if possible < od {
-                        other.distance = Known(us.distance.unwrap() + 1);
-                        return true;
-                    }
-                    return false;
-                }
-            } else {
-                false
-            }
-        }
-
-        // look up
-        if let Some((y, x)) = up(current.index) {
-            let up = &mut grid[y][x];
-            if climbable(current, up) {
-                queue.insert(*up);
-            }
-        }
-        // look down
-        if let Some((y, x)) = down(current.index) {
-            let down = &mut grid[y][x];
-            if climbable(current, down) {
-                queue.insert(*down);
-            }
-        }
-        // look left
-        if let Some((y, x)) = left(current.index) {
-            let left = &mut grid[y][x];
-            if climbable(current, left) {
-                queue.insert(*left);
-            }
-        }
-        // look right
-        if let Some((y, x)) = right(current.index) {
-            let right = &mut grid[y][x];
-            if climbable(current, right) {
-                queue.insert(*right);
-            }
-        }
-    }
-
-    a_paths.sort_unstable();
-    a_paths[0]
+    })
 }
 
 #[cfg(test)]
